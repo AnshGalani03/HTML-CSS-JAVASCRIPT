339d567e4d568b036dca184c055563a188dfec2f
Ansh Galani <85383708+AnshGalani@users.noreply.github.com>
2022-04-12 09:19:45 +0530

    Add files via upload

---
+++ Calaulator/calculator.js
@@ -0,0 +1,45 @@
+let op=false;
+function setValue(val,type="number")
+{
+
+    if(type==="number")
+    {
+        document.querySelector(".inp").value+=val;
+        op=false;
+    }
+
+    if(type==="operator" && op===false)
+    {
+        document.querySelector(".inp").value+=val;
+        op=true;
+    }
+
+    
+}
+
+function calculate()
+{
+    var data=document.querySelector(".inp").value;
+    let answer=eval(data);
+    document.querySelector(".inp").value=answer;
+}
+
+function clearAll()
+{
+    document.querySelector(".inp").value="";
+}
+
+function backSpace()
+{
+    let data=document.querySelector(".inp").value;
+    let newData=data.substring(0,data.length-1);
+    document.querySelector(".inp").value=newData;
+    op=false;
+}
+
+function squareRoot()
+{
+    let data=document.querySelector(".inp").value;
+    let answer=Math.sqrt(Number(data));
+    document.querySelector(".inp").value=answer;
+}
